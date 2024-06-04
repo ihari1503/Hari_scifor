85b0b4246f4e094e871b98e63654bc5b28b1addc
ihari1503 <harikrishnand1503@gmail.com>
2024-06-04 18:09:42 +0530

    Create merge strings.py

---
+++ Python/Assignment/merge strings.py
@@ -0,0 +1,23 @@
+#Question Merge strings Alternatively
+
+#code
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        if len(word1) < len(word2): 
+
+           
+            for i in range(len(word1)): 
+                new_word =new_word + word1[i] + word2[i] 
+
+           
+            return (new_word + word2[len(word1):])
+
+        else:
+            for i in range(len(word2)):
+                new_word =new_word + word1[i] + word2[i]
+            return (new_word + word1[len(word2):])
