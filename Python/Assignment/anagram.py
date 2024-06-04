5a375481a3e9bb947659a2905b0d11fde940af54
ihari1503 <harikrishnand1503@gmail.com>
2024-06-04 18:11:44 +0530

    Create anagram.py

---
+++ Python/Assignment/anagram.py
@@ -0,0 +1,21 @@
+#Question Checck whether it is a valid anagram or not
+
+#Code
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_sorted = sorted(s)
+        t_sorted = sorted(t)
+
+        if len(s) == len(t):
+            if s_sorted == t_sorted:
+                return True
+            else:
+                return False
+        else:
+            return False
