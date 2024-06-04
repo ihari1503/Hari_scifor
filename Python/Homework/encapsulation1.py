7f280090a52f7541fa95a37a94b6f13a04b555ce
ihari1503 <harikrishnand1503@gmail.com>
2024-06-04 18:31:10 +0530

    Create encapsulation1.py

---
+++ Python/Homework/encapsulation1.py
@@ -0,0 +1,15 @@
+class Car:
+  def __init__(self,brand,model,year):
+    self.__brand
+    self.model
+    self.year
+  def getbrand(self):
+    return.self.__brand
+    
+  def acceleration(self):
+    print("The {self.model} is accelerating)
+          
+  mycar = Car(Tesla,Model V,2023)
+
+  print(mycar.model)
+  print(mycar.getbrand())
