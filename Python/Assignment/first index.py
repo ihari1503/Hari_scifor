14eefb39da0550df6f816037d1769cf4a033c1a1
ihari1503 <harikrishnand1503@gmail.com>
2024-06-05 12:06:00 +0530

    Create first index.py

---
+++ Python/Assignment/first index.py
@@ -0,0 +1,12 @@
+#Question  First occurence of a string
+
+#Code
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        return haystack.find(needle)
